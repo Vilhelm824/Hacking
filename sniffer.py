dd88384a1ec83491313c73377887854f353512c8
Will Fischer <willhfischer@gmail.com>
2022-01-11 06:13:47 +0000

    little bit of work on sniffer

--- sniffer.py
+++ sniffer.py
@@ -1,12 +1,16 @@
-#! /usr/bin/python3
+#! /usr/bin/python
+
 import scapy.all as scapy
+from scapy.layers import http
 
 def sniff(interface):
-	scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
+	# set store to false so memory isn't overwhelmed, prn calls given function every time this is executed
+	scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet) 
 
 
 def process_sniffed_packet(packet):
-	print(packet)
+	if packet.haslayer(http.HTTPRequest):
+		print(packet.show)
 
 
 sniff("eth0")
